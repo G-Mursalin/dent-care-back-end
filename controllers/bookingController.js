d5481e19ff83a5496c5ab14c74e44360fd91fa17
Golam Mursalin <golammursalin309@gmail.com>
2023-02-12 15:10:49 +0600

    limit booking per person per date per service

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -14,10 +14,26 @@ const getAllBookings = catchAsync(async (req, res) => {
 });
 
 const createABooking = catchAsync(async (req, res) => {
-  const newBooking = await Booking.create(req.body);
-  res
-    .status(201)
-    .send({ status: "Booked Successfully", data: { booking: newBooking } });
+  const { serviceName, patientEmail, appointmentDate } = req.body;
+
+  // Limit Booking per user per treatment per day
+  const isAlreadyBooked = await Booking.find({
+    serviceName,
+    patientEmail,
+    appointmentDate,
+  });
+  if (!(isAlreadyBooked.length === 0)) {
+    res.status(201).send({
+      status: `You already have an appointment on ${appointmentDate}`,
+      data: { booking: isAlreadyBooked },
+    });
+  } else {
+    // Create booking
+    const newBooking = await Booking.create(req.body);
+    res
+      .status(201)
+      .send({ status: "Booked Successfully", data: { booking: newBooking } });
+  }
 });
 
 module.exports = { createABooking, getAllBookings };
