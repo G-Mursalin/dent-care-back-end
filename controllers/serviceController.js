c3704f8a7672a85e298ca4fdc448fb9215b41ecc
Golam Mursalin <golammursalin309@gmail.com>
2023-02-12 12:29:41 +0600

    aggregation pipeline added for booking and srrvices collections

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -2,12 +2,54 @@ const Service = require("../models/serviceModel");
 const { catchAsync } = require("../utils/catchAsync");
 
 const getAllServices = catchAsync(async (req, res) => {
-  const services = await Service.find();
+  const date = req.query.date;
+  const services_aggregate = await Service.aggregate([
+    {
+      $lookup: {
+        from: "bookings",
+        localField: "name",
+        foreignField: "serviceName",
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $eq: ["$appointmentDate", date],
+              },
+            },
+          },
+        ],
+        as: "booked",
+      },
+    },
+    {
+      $project: {
+        name: 1,
+        slots: 1,
+        price: 1,
+        booked: {
+          $map: {
+            input: "$booked",
+            as: "book",
+            in: "$$book.slot",
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        name: 1,
+        price: 1,
+        slots: {
+          $setDifference: ["$slots", "$booked"],
+        },
+      },
+    },
+  ]);
 
   res.status(200).send({
     status: "success",
-    results: services.length,
-    data: { services },
+    results: services_aggregate.length,
+    data: { services: services_aggregate },
   });
 });
 
